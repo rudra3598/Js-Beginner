a596dc5faea74898f47ed20fe6dba4247630f1f6
Rudra Patel <63511402+rudra3598@users.noreply.github.com>
2024-03-19 06:02:20 +0000

    modified:   Day-1_BasicJS/Day-1-2.js

--- Day-1_BasicJS/Day-1-2.js
+++ Day-1_BasicJS/Day-1-2.js
@@ -1,2 +1,44 @@
 // In this Part we Mostly Gonna learn about Datatypes and ECMA standards
 
+
+"use strict"; // Treat all JS code as newer varsion
+
+// alert(1+1) //we are using nodejs, not browser so this one is don't works.
+
+console.log(3 + 3)
+
+
+console.log("Hitesh");
+
+let name ="Hitesh"
+let age = 18
+let isLoggedIn = false
+
+
+
+// Here is The datatypes
+// number => 2 to power 53
+// bigInt
+// String => ""
+// boolean => True/false
+// null => Standalone Value
+// undefined =>  didn't provided any value
+// symbol => Unique
+
+
+
+// Object 
+
+
+
+// through typeof we can check the datatype of variables
+console.log("Here is the data type" + '\n');
+// We can use  '\n' for break the row. start new line
+console.log("Name is " + typeof name + " Datatype" + '\n'); 
+
+console.log("isLoggedIn is " + typeof isLoggedIn + " Datatype" + '\n');
+console.log("123 is " + typeof 123 + " Datatype" + '\n');
+console.log("rudra is " + typeof "rudra" + " Datatype"+ '\n');
+console.log("false is " + typeof false + " Datatype"+ '\n');
+console.log("null is " + typeof null + " Datatype"+ '\n');
+console.log("undefined is " + typeof undefined + " Datatype"+ '\n');
