ac0a198bc232b5733eb7c339434a36558709aaee
Rudra Patel <63511402+rudra3598@users.noreply.github.com>
2024-03-27 18:55:38 +0000

    new file:   Day-2_/Day-2-1.js

---
+++ Day-2_/Day-2-1.js
@@ -0,0 +1,21 @@
+// Let's see Opeerations.
+
+let Value = 3
+let negValue = -Value
+console.log(negValue);
+
+console.log(3+3);
+console.log(3-3);
+console.log(3*3);
+console.log(3**3);
+console.log(2/3);
+console.log(3%4);
+
+
+
+
+
+let str1 = "Hello"
+let str2 = "Rudra"
+
+console.log(str1 +  " " +str2);
